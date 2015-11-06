3294e45e371d6bf3885e1e0f5dc69adaa0792968
bhargav <bhargav.rao448@gmail.com>
2015-11-07 01:18:03 +0530

    stuff

--- js/custom.js
+++ js/custom.js
@@ -5,30 +5,52 @@ function changediv()
 	if (txtarea.value=="")
 	    output.innerHTML = '<span class="grey ital">Expect Output here<span>'
 	else
-	    output.innerHTML = txtarea.value
+	{
+	    splits = txtarea.value.split(' ')
+	    list = dataObj()
+	    for(var i=0;i<splits.length;i++)
+	    {
+	        var word = splits[i]
+	        for (var key in list)
+	        {
+	            for (var j = 0; j<list[key].length;j++)
+	            {
+	                
+	                if (list[key][j].toLowerCase() == word.toLowerCase())
+	                {
+	                    var fillvalue = "<span class='"+key+"' title='Belongs to the "+key+" domain'>"+word+"</span>"
+	                    splits[i] = fillvalue
+	                }
+	            }
+	        }
+	    }
+	    data = splits.join(' ')
+	    
+	    output.innerHTML = data
+	}
 }
 
 function dataObj()
 {
-    return {"KNOWLEDGE":["Write", "List", "Label", "Name", "State", "Define", "Count", "Describe", "Draw", "Find", "Identify", "Match", 
+    return {"knowledge":["Write", "List", "Label", "Name", "State", "Define", "Count", "Describe", "Draw", "Find", "Identify", "Match", 
                         "Quote", "Recall", "Recite", "Sequence", "Tell", "Arrange", "Duplicate", "Memorize", "Order", "Outline", 
                         "Recognize", "Relate", "Repeat", "Reproduce", "Select", "Choose", "Copy", "How", "Listen", "Locate",
 						"Memorise", "Observe", "Omit", "Read", "Recognise", "Record", "Remember", "Retell", "Show", "Spell",
 						"Trace", "What", "When", "Where", "Which", "Who", "Why"],
-          "COMPREHENSION":["Explain", "Summarize", "Paraphrase", "Describe", "Illustrate", "Conclude", "Demonstrate", "Discuss",
+          "comprehension":["Explain", "Summarize", "Paraphrase", "Describe", "Illustrate", "Conclude", "Demonstrate", "Discuss",
 						   "Generalize", "Identify", "Interpret", "Predict", "Report", "Restate", "Review", "Tell", "Classify",
 						   "Convert", "Defend", "Distinguish", "Estimate", "Express", "Extend", "Give example", "Indicate",
 						   "Infer", "Locate", "Recognize", "Rewrite", "Select", "Translate", "Ask", "Cite", "Compare",
 						   "Contrast", "Generalise", "Give examples", "Match", "Observe", "Outline", "Purpose", "Relate",
 						   "Rephrase", "Show", "Summarise"],
-          "APPLICATION":["Use", "Compute", "Solve", "Demonstrate", "Apply", "Construct", "Change", "Choose", "Dramatize", "Interview",
+          "application":["Use", "Compute", "Solve", "Demonstrate", "Apply", "Construct", "Change", "Choose", "Dramatize", "Interview",
 						 "Prepare", "Produce", "Select", "Show", "Transfer", "Discover", "Employ", "Illustrate",
 						 "Interpret", "Manipulate","Modify", "Operate", "Practice", "Predict", "Relate schedule", "Sketch",
 						 "Use write", "Act", "Administer", "Associate", "Build", "Calculate", "Categorise", "Classify",
 						 "Connect", "Correlation", "Develop", "Dramatise", "Experiment", "With", "Group", "Identify",
 						 "Link", "Make use of", "Model", "Organise", "Perform", "Plan", "Relate", "Represent", "Simulate",
 						 "Summarise", "Teach", "Translate"],
-          "ANALYSIS":["Analyse", "Categorize", "Compare", "Contrast", "Separate", "Characterize", "Classify", "Debate", "Deduce", 
+          "analysis":["Analyse", "Categorize", "Compare", "Contrast", "Separate", "Characterize", "Classify", "Debate", "Deduce", 
 					  "Diagram", "Differentiate", "Discriminate", "Distinguish", "Examine", "Outline", "Relate", "Research", 
 					  "Appraise", "Breakdown", "Calculate", "Criticize", "Derive", "Experiment", "Identify", "Illustrate", 
 					  "Infer", "Interpret", "Model", "Outline", "Point out", "Question", "Select", "Subdivide", "Test", 
@@ -37,7 +59,7 @@ function dataObj()
 					  "In-depth", "Discussion", "Inference", "Inspect", "Investigate", "Isolate", "List", "Motive", "Omit", 
 					  "Order", "Organise", "Point out", "Prioritize", "Rank", "Reason", "Relationships", "Reorganise", "See", 
 					  "Similar to", "Simplify", "Survey", "Take part in", "Test for", "Theme", "Comparing"],
-          "SYNTHESIS":["Create", "Design", "Hypothesize", "Invent", "Develop", "Compose", "Construct", "Integrate", "Make",
+          "synthesis":["Create", "Design", "Hypothesize", "Invent", "Develop", "Compose", "Construct", "Integrate", "Make",
 					   "Organize", "Perform", "Plan", "Produce", "Propose", "Rewrite", "Arrange", "Assemble", "Categorize", 
 					   "Collect", "Combine", "Comply", "Devise", "Explain", "Formulate", "Generate", "Prepare", "Rearrange",
 					   "Reconstruct", "Relate", "Reorganize", "Revise", "Set up", "Summarize", "Synthesize", "Tell", "Write", 
@@ -46,7 +68,7 @@ function dataObj()
 					   "Improve", "Innovate", "Make up", "Maximise", "Minimise", "Model", "Modify", "Original", "Originate",
 					   "Predict", "Reframe", "Simplify", "Solve", "Speculate", "Substitute", "Suppose", "Tabulate", "Test", 
 					   "Theorise", "Think", "Transform", "Visualise"],
-          "EVALUATION":["Judge", "Recommend", "Critique", "Justify", "Appraise", "Argue", "Assess", "Choose", "Conclude", 
+          "evaluation":["Judge", "Recommend", "Critique", "Justify", "Appraise", "Argue", "Assess", "Choose", "Conclude", 
 						"Decide", "Evaluate", "Predict", "Prioritize", "Prove", "Rank", "Rate", "Select", "Attach", "Compare", 
 						"Contrast", "Defend", "Describe", "Discriminate", "Estimate", "Explain", "Interpret", "Relate",
 						"Summarize", "Support", "Value", "Agree", "Award", "Bad", "Consider", "Convince", "Criteria", 
